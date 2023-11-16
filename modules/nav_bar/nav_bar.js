1cb34122a9640a81d29eb5ab1a27b8142beb1f64
Zecr <z.edwin@outlook.com>
2023-11-16 02:32:30 -0800

    Some stuff

---
+++ modules/nav_bar/nav_bar.js
@@ -0,0 +1,39 @@
+// <script src="nav_bar/nav_bar.js" data-id="Home"></script>
+
+// Get the id of the current page
+var id = document.currentScript.getAttribute("data-id");
+
+// Add CSS dependency to the page
+document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="modules/nav_bar/nav_bar.css">`);
+
+// Pages to be added to the nav bar
+var pages = {
+    Home: "pages/home_page/home.html",
+    About: "pages/about_page/about.html",
+    Contact: "pages/contact_page/contact.html",
+};
+
+// Add nav items to the nav bar
+for (var key in pages) {
+    if (id != key) {
+        document.write(`<a class="nav_item" hx-get="${pages[key]}" hx-target="main"> ${key} </a>`);
+    } else {
+        document.write(`<a class="nav_item active" hx-get="${pages[key]}" hx-target="main"> ${key} </a>`);
+    }
+}
+
+// Add listener to nav items, when clicked, update the nav bar
+let nav_bar = document.currentScript.parentElement;
+
+nav_bar.addEventListener("click", (e) => {
+    Array.from(nav_bar.children).forEach((link) => {
+        if (link.text == e.target.text) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
+
+// Remove the script tag
+document.currentScript.remove();
